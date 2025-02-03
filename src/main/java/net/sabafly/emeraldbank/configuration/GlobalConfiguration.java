c53333b36add6515d796ee0621d3be826ca5d409
ikafly144 <support@sabafly.net>
2025-02-03 21:09:03 +0900

    Add comments to configuration fields for improved clarity

--- src/main/java/net/sabafly/emeraldbank/configuration/GlobalConfiguration.java
+++ src/main/java/net/sabafly/emeraldbank/configuration/GlobalConfiguration.java
@@ -2,6 +2,7 @@ package net.sabafly.emeraldbank.configuration;
 
 import net.sabafly.emeraldbank.configuration.type.IntOr;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Comment;
 import org.spongepowered.configurate.objectmapping.meta.Setting;
 
 @SuppressWarnings({"FieldCanBeLocal", "FieldMayBeFinal", "InnerClassMayBeStatic"})
@@ -20,10 +21,19 @@ public class GlobalConfiguration extends ConfigurationPart {
     }
 
     @Setting(Configuration.VERSION_FIELD)
+    @Comment("DO NOT CHANGE THIS VALUE!")
     public int version = CURRENT_VERSION;
 
+    @Comment("The cost for creating a bank.")
     public IntOr.Disabled payCost = IntOr.Disabled.DISABLED;
 
+    @Comment("""
+            ################################################################
+            #                                                              #
+            #  Banking                                                     #
+            #                                                              #
+            ################################################################
+            """)
     public Banking banking = new Banking();
 
     @ConfigSerializable
@@ -31,8 +41,16 @@ public class GlobalConfiguration extends ConfigurationPart {
 
         public Banking() {}
 
+        @Comment("Enable or disable banking.")
         public boolean enabled = true;
 
+        @Comment("""
+                ################################################################
+                #                                                              #
+                #  Banking Tax                                                 #
+                #                                                              #
+                ################################################################
+                """)
         public BankingTax tax = new BankingTax();
 
         @ConfigSerializable
@@ -40,15 +58,26 @@ public class GlobalConfiguration extends ConfigurationPart {
 
             public BankingTax() {}
 
+            @Comment("The cost for creating a bank.")
             public IntOr.Disabled createCost = IntOr.Disabled.DISABLED;
+            @Comment("The cost for creating a bank.")
             public IntOr.Disabled addMemberCost = IntOr.Disabled.DISABLED;
+            @Comment("The cost for removing a member from a bank.")
             public IntOr.Disabled transferBankCost = IntOr.Disabled.DISABLED;
+            @Comment("The cost for sending from a bank to another bank.")
             public IntOr.Disabled depositCost = IntOr.Disabled.DISABLED;
+            @Comment("The cost for withdrawing from a bank.")
             public IntOr.Disabled withdrawCost = IntOr.Disabled.DISABLED;
+            @Comment("The cost for paying from a bank to a player.")
             public IntOr.Disabled payCost = IntOr.Disabled.DISABLED;
         }
     }
 
+    @Comment("""
+            The default destination for payments.
+            INVENTORY: The player's inventory.
+            WALLET: The player's wallet.
+            """)
     public DefaultDestination defaultDestination = DefaultDestination.INVENTORY;
 
     public enum DefaultDestination {
