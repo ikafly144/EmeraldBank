b9cb61ec88f1f9883bae58b0e6d9bf5a71c108c8
ikafly144 <ikafly144@sabafly.net>
2026-05-04 10:09:19 +0900

    refactor: migrate build configuration to Kotlin DSL and enhance library loading

---
+++ build.gradle.kts
@@ -0,0 +1,259 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import net.minecrell.pluginyml.GeneratePluginDescription
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+import net.minecrell.pluginyml.paper.PaperPluginDescription
+import xyz.jpenilla.runpaper.task.RunServer
+
+plugins {
+    java
+    id("de.eldoria.plugin-yml.paper") version "0.9.0"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"
+    id("xyz.jpenilla.run-paper") version "3.0.2"
+    id("com.gradleup.shadow") version "9.3.1"
+}
+
+allprojects {
+    group = "net.sabafly"
+    version = "1.2.1"
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://jitpack.io")
+    }
+    maven {
+        name = "papermc-repo"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        name = "sonatype"
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+    maven {
+        url = uri("https://repo.extendedclip.com/releases/")
+    }
+    maven {
+        name = "essentialsxReleases"
+        url = uri("https://repo.essentialsx.net/releases")
+    }
+}
+
+dependencies {
+    paperLibrary("com.h2database:h2:2.4.240")
+    paperLibrary("com.mysql:mysql-connector-j:9.7.0")
+    implementation("com.zaxxer:HikariCP:7.0.2")
+    implementation("commons-dbutils:commons-dbutils:1.8.1")
+    implementation("org.spongepowered:configurate-yaml:4.3.0-SNAPSHOT")
+    implementation("com.vdurmont:semver4j:3.1.0")
+
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7.1") {
+        isTransitive = false
+    }
+    compileOnly("me.clip:placeholderapi:2.12.2") {
+        isTransitive = false
+    }
+    compileOnly("com.github.Jikoo:OpenInv:5.3.1")
+    compileOnly("net.essentialsx:EssentialsX:2.21.2") {
+        isTransitive = false
+    }
+
+    compileOnly("org.projectlombok:lombok:1.18.46")
+    annotationProcessor("org.projectlombok:lombok:1.18.46")
+    paperweight.paperDevBundle("26.1.2.build.+")
+}
+
+paper {
+    name = rootProject.name
+    main = "net.sabafly.emeraldbank.EmeraldBank"
+    apiVersion = "1.21.6"
+    website = "https://modrinth.com/plugin/emeraldbank"
+    bootstrapper = "net.sabafly.emeraldbank.EmeraldBootstrapper"
+    loader = "net.sabafly.emeraldbank.EmeraldLoader"
+    generateLibrariesJson = true
+    foliaSupported = true
+
+    bootstrapDependencies {
+        register("Essentials") {
+            required = false
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+            joinClasspath = true
+        }
+    }
+    serverDependencies {
+        register("Vault") {
+            required = true
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+            joinClasspath = true
+        }
+        register("OpenInv") {
+            required = false
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+            joinClasspath = true
+        }
+        register("PlaceholderAPI") {
+            required = false
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+            joinClasspath = true
+        }
+        register("Essentials") {
+            required = false
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+            joinClasspath = true
+        }
+    }
+
+    permissions {
+        register("emeraldbank.bypass") {
+            default = BukkitPluginDescription.Permission.Default.FALSE
+            description = "Allows the player to bypass the EmeraldBank command"
+            childrenMap = mapOf(
+                "emeraldbank.bypass.cost" to false,
+                "emeraldbank.bypass.deposit" to false,
+                "emeraldbank.bypass.member" to false,
+                "emeraldbank.bypass.owner" to false
+            )
+        }
+        register("emeraldbank.admin") {
+            default = BukkitPluginDescription.Permission.Default.OP
+            description = "Allows the player to use all EmeraldBank command"
+            childrenMap = mapOf(
+                "emeraldbank.balance.all" to true,
+                "emeraldbank.wallet.balance.all" to true,
+                "emeraldbank.reload" to true,
+                "emeraldbank.currency.rate.set" to true,
+                "emeraldbank.default" to true
+            )
+        }
+        register("emeraldbank.default") {
+            default = BukkitPluginDescription.Permission.Default.TRUE
+            description = "Allows the player to use the EmeraldBank command"
+            childrenMap = mapOf(
+                "emeraldbank.balance" to true,
+                "emeraldbank.pay" to true,
+                "emeraldbank.leaderboard" to true,
+                "emeraldbank.wallet" to true,
+                "emeraldbank.banking" to true,
+                "emeraldbank.currency" to true
+            )
+        }
+        register("emeraldbank.wallet") {
+            default = BukkitPluginDescription.Permission.Default.TRUE
+            description = "Allows the player to use the EmeraldBank command"
+            childrenMap = mapOf(
+                "emeraldbank.wallet.balance" to true,
+                "emeraldbank.wallet.add" to true,
+                "emeraldbank.wallet.withdraw" to true
+            )
+        }
+        register("emeraldbank.banking") {
+            default = BukkitPluginDescription.Permission.Default.TRUE
+            description = "Allows the player to use the EmeraldBank command"
+            childrenMap = mapOf(
+                "emeraldbank.banking.account" to true,
+                "emeraldbank.banking.withdraw" to true,
+                "emeraldbank.banking.deposit" to true,
+                "emeraldbank.banking.balance" to true,
+                "emeraldbank.banking.send" to true,
+                "emeraldbank.banking.pay" to true
+            )
+        }
+        register("emeraldbank.banking.account") {
+            default = BukkitPluginDescription.Permission.Default.TRUE
+            description = "Allows the player to use the EmeraldBank command"
+            childrenMap = mapOf(
+                "emeraldbank.banking.account.create" to true,
+                "emeraldbank.banking.account.delete" to true,
+                "emeraldbank.banking.account.add" to true,
+                "emeraldbank.banking.account.remove" to true,
+                "emeraldbank.banking.account.list" to true,
+                "emeraldbank.banking.account.transfer" to true
+            )
+        }
+        register("emeraldbank.currency") {
+            default = BukkitPluginDescription.Permission.Default.TRUE
+            description = "Allows the player to use the EmeraldBank command"
+            childrenMap = mapOf(
+                "emeraldbank.currency.rate" to true,
+                "emeraldbank.currency.exchange" to true,
+                "emeraldbank.currency.exchange.*" to true
+            )
+        }
+        register("emeraldbank.currency.rate") {
+            default = BukkitPluginDescription.Permission.Default.TRUE
+            description = "Allows the player to use the EmeraldBank command"
+            childrenMap = mapOf(
+                "emeraldbank.currency.rate.get" to true
+            )
+        }
+    }
+}
+
+tasks.named<GeneratePluginDescription>("generatePaperPluginDescription") {
+    useDefaultCentralProxy()
+}
+
+val targetJavaVersion = 25
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+
+    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+        options.release.set(targetJavaVersion)
+    }
+}
+
+val shadowJarTask = tasks.named<ShadowJar>("shadowJar") {
+    archiveBaseName = project.name
+    archiveClassifier = ""
+    archiveVersion = project.version.toString()
+
+    dependencies {
+        exclude(dependency("org.slf4j:slf4j-api"))
+        exclude(dependency("net.kyori:option"))
+    }
+
+    minimize()
+
+    relocate("org.spongepowered.configurate", "net.sabafly.configurate")
+    relocate("io.leangen.geantyref", "net.sabafly.geantyref")
+
+    relocate("com.zaxxer.hikari", "net.sabafly.libs.com.zaxxer.hikari")
+    relocate("org.apache.commons.dbutils", "net.sabafly.libs.org.apache.commons.dbutils")
+
+    relocate("com.vdurmont.semver4j", "net.sabafly.libs.com.vdurmont.semver4j")
+}
+
+tasks.build {
+    dependsOn(shadowJarTask)
+}
+
+runPaper.folia.registerTask {
+    downloadPlugins {
+        modrinth("openinv", "5.3.1")
+        github("SirBlobman", "Vault-Folia", "v1.7.3-folia", "Vault-1.7.3.jar")
+        modrinth("foliaperms", "1.12.0")
+        modrinth("placeholderapi", "2.12.2")
+    }
+    runDirectory = file("run/folia")
+    minecraftVersion("1.21.11")
+}
+
+tasks.named<RunServer>("runServer") {
+    downloadPlugins {
+        modrinth("openinv", "5.3.1")
+        github("MilkBowl", "Vault", "1.7.3", "Vault.jar")
+        modrinth("luckperms", "v5.5.17-bukkit")
+        modrinth("placeholderapi", "2.12.2")
+    }
+    runDirectory = file("run/paper")
+    minecraftVersion("26.1.2")
+}
--- src/main/java/net/sabafly/emeraldbank/EmeraldLoader.java
+++ src/main/java/net/sabafly/emeraldbank/EmeraldLoader.java
@@ -1,5 +1,6 @@
 package net.sabafly.emeraldbank;
 
+import com.google.gson.Gson;
 import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
 import io.papermc.paper.plugin.loader.PluginLoader;
 import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
@@ -8,26 +9,48 @@ import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.stream.Stream;
 
 @SuppressWarnings({"unused", "UnstableApiUsage"})
 public class EmeraldLoader implements PluginLoader {
     @Override
     public void classloader(@NotNull PluginClasspathBuilder classpathBuilder) {
         MavenLibraryResolver resolver = new MavenLibraryResolver();
-        List<String> dependencies = List.of(
-                "com.h2database:h2:2.4.240",
-                "com.mysql:mysql-connector-j:9.6.0"
-        );
-
-        for (String dependency : dependencies) {
-            resolver.addDependency(new Dependency(new DefaultArtifact(dependency), "compile"));
-        }
+        PluginLibraries pluginLibraries = loadLibraries();
+        pluginLibraries.asDependencies().forEach(resolver::addDependency);
 
         resolver.addRepository(new RemoteRepository.Builder("central", "default", MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR).build());
         resolver.addRepository(new RemoteRepository.Builder("papermc", "default", "https://repo.papermc.io/repository/maven-public/").build());
         resolver.addRepository(new RemoteRepository.Builder("sonatype", "default", "https://oss.sonatype.org/content/groups/public/").build());
 
         classpathBuilder.addLibrary(resolver);
+    }
+
+    private PluginLibraries loadLibraries() {
+        try (var in = EmeraldLoader.class.getResourceAsStream("/paper-libraries.json")) {
+            if (in == null) {
+                throw new IllegalStateException("paper-libraries.json is missing from plugin resources");
+            }
+
+            PluginLibraries pluginLibraries = new Gson()
+                    .fromJson(new InputStreamReader(in, StandardCharsets.UTF_8), PluginLibraries.class);
+            if (pluginLibraries == null) {
+                throw new IllegalStateException("paper-libraries.json is empty");
+            }
+            return pluginLibraries;
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to read paper-libraries.json", e);
+        }
+    }
+
+    private record PluginLibraries(List<String> dependencies) {
+        private Stream<Dependency> asDependencies() {
+            return (dependencies == null ? List.<String>of() : dependencies).stream()
+                    .map(coordinates -> new Dependency(new DefaultArtifact(coordinates), "compile"));
+        }
     }
 }
